116dd7fda69775ee72bd303d95f6b874f444b415
Muhammadqodir <muhammadqodirummatov31@gmail.com>
2024-03-14 21:48:07 +0200

    protected route change

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,17 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-
-const protectedRoutes = ["/auth/signup"];
-const protectedRoutes2 = ["/auth/signin"];
-
+const protectedRoutes = ["/auth/signup", "/auth/signin"];
 
 export default function middleware(req: NextRequest) {
-  let isAuthenticated =  req.cookies.get('loggedin')   
-  if (isAuthenticated && (protectedRoutes.includes(req.nextUrl.pathname) || protectedRoutes2.includes(req.nextUrl.pathname ))) {
+  let isAuthenticated = req.cookies.get('loggedin');
+  
+  if (!isAuthenticated && protectedRoutes.includes(req.nextUrl.pathname)) {
+    return NextResponse.next();
+  } else if (isAuthenticated && protectedRoutes.includes(req.nextUrl.pathname)) {
     const absoluteURL = new URL("/", req.nextUrl.origin);
     return NextResponse.redirect(absoluteURL.toString());
   }
+
+  return NextResponse.next();
 }
