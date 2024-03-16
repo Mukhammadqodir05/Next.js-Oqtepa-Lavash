ae1de6c1f309ed16edd9c380641dc11de2361ce8
Muhammadqodir <muhammadqodirummatov31@gmail.com>
2024-03-16 18:46:15 +0200

    Navbar edit 2

--- app/componets/navbar.tsx
+++ app/componets/navbar.tsx
@@ -44,9 +44,9 @@ const Navbar = () => {
             {isNavOpen && (
               <div className='md:hidden fixed top-0 left-0 w-full h-screen bg-black bg-opacity-80 z-50 transform transition-transform duration-300 ease-in-out p-1 '>
                 <div className='flex items-center w-full justify-between'>
-                <div className='ml-4 mt-[1px] cursor-pointer'>
+                <Link href='/' className='ml-4 mt-[1px] cursor-pointer'>
                     <Logo />
-                </div>
+                </Link>
                 <div className='mr-2 cursor-pointer hover:bg-[#595959] rounded-full p-1'>
                     <MdClose title='close' onClick={handleNavbar} size={30}/>
                   </div>
